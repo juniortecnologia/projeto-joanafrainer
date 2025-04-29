cdbf4718d17b39e7c24a800bee21055c166fda31
juniortecnologia <junioralmeidatecnologia@gmail.com>
2025-04-29 11:39:06 -0300

    reconstruindo-o-repo

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('sobre').addEventListener('click', function(event) {
+    event.preventDefault(); // Previne o comportamento padrão do link
+    var section = document.getElementById('joana-frainer');
+    section.style.display = (section.style.display === 'none') ? 'flex' : 'none'; // Alterna entre none e block
+  });
+  

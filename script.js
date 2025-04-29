f7bed1241071ca57b620f2a651c4f87bb91ffa77
juniortecnologia <junioralmeidatecnologia@gmail.com>
2025-04-29 17:25:35 -0300

    29/04/2025

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('sobre').addEventListener('click', function(event) {
-    event.preventDefault(); // Previne o comportamento padrão do link
+   // event.preventDefault(); // Previne o comportamento padrão do link
     var section = document.getElementById('joana-frainer');
-    section.style.display = (section.style.display === 'none') ? 'flex' : 'none'; // Alterna entre none e block
+    section.style.display = (section.style.display === 'none') ? 'flex' : 'flex'; // Alterna entre none e block
   });
   
